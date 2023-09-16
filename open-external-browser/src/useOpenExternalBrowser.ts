08c05397634bdb9fc8376987c95c4657e7a8ce59
hyesungoh <haesungoh414@gmail.com>
2023-09-16 20:06:43 +0900

    docs: tsdoc at use open external browser

--- open-external-browser/src/useOpenExternalBrowser.ts
+++ open-external-browser/src/useOpenExternalBrowser.ts
@@ -4,9 +4,28 @@ import { type Where } from './constants';
 import { openExternalBrowser } from './openExternalBrowser';
 
 interface Props {
+  /**
+   * @description Decide in what environment to open it
+   * @example 'all' - Open in all environments
+   * @example ['kakao', 'line'] - Open in ['kakao', 'line']
+   * @example 'kakao' - Open in 'kakao'
+   */
   where: Where;
+  /**
+   * @description URL to open
+   * @default location.href
+   * @example 'https://github.com'
+   */
   to?: string;
 }
+
+/**
+ * @description Open the URL in the environment specified by where
+ * @note run in effect
+ * @example useOpenExternalBrowser({ where: 'all', to: 'https://github.com' })
+ * @example useOpenExternalBrowser({ where: ['kakao', 'line'] })
+ * @example useOpenExternalBrowser({ where: 'kakao' })
+ */
 export function useOpenExternalBrowser({ where, to }: Props) {
   useEffect(() => {
     openExternalBrowser({ where, to });
